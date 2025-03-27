69e8ad4c38a4b5da2c39d224bec6b6ed5b2a7723
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-03-27 20:24:54 -0300

    feat: initial config

---
+++ AppleInvites/View/IntroPage.swift
@@ -0,0 +1,18 @@
+//
+//  IntroPage.swift
+//  AppleInvites
+//
+//  Created by Luis Filipe Pedroso on 27/03/25.
+//
+
+import SwiftUI
+
+struct IntroPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroPage()
+}
