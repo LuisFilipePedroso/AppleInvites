d5768fe4c61dcc52c246037f43cd915aeb9248a4
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-03-27 20:56:06 -0300

    feat: create carousel list item

--- AppleInvites/View/IntroPage.swift
+++ AppleInvites/View/IntroPage.swift
@@ -41,6 +41,27 @@ struct IntroPage: View {
         }
     }
     
+    @ViewBuilder
+    private func CarouselCardView(_ card: Card) -> some View {
+        GeometryReader {
+            let size = $0.size
+            
+            Image(card.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: size.width, height: size.height)
+                .clipShape(.rect(cornerRadius: 20))
+                .shadow(color: .black.opacity(0.4), radius: 10, x: 1, y: 0)
+        }
+        .frame(width: 220)
+        .scrollTransition(.interactive.threshold(.centered), axis: .horizontal) { content,
+            phase in
+            content
+                .offset(y: phase == .identity ? -10 : 0)
+                .rotationEffect(.degrees(phase.value * 5), anchor: .bottom)
+        }
+    }
+    
 }
 
 #Preview {
