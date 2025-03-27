d26b4ee27ee6212226c222a445c7185f09815a17
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-03-27 20:30:36 -0300

    feat: add IntroPage and create blur effect

--- AppleInvites/View/IntroPage.swift
+++ AppleInvites/View/IntroPage.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct IntroPage: View {
+    @State private var activeCard: Card? = cards.first
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AmbientBackground()
+        }
     }
+    
+    @ViewBuilder
+    private func AmbientBackground() -> some View {
+        GeometryReader {
+            let size = $0.size
+            
+            ZStack {
+                ForEach(cards) { card in
+                    Image(card.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .ignoresSafeArea()
+                        .frame(width: size.width, height: size.height)
+                        .opacity(activeCard?.id == card.id ? 1 : 0)
+                }
+                
+                Rectangle()
+                    .fill(.black.opacity(0.45))
+                    .ignoresSafeArea()
+            }
+            .compositingGroup()
+            .blur(radius: 90, opaque: true)
+            .ignoresSafeArea()
+        }
+    }
+    
 }
 
 #Preview {
