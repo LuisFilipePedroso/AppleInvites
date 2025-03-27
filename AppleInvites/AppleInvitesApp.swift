aacf09cd75491f4a2305f6925c3630b68a1edccf
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-03-27 20:18:14 -0300

    Initial Commit

---
+++ AppleInvites/AppleInvitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleInvitesApp.swift
+//  AppleInvites
+//
+//  Created by Luis Filipe Pedroso on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleInvitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
